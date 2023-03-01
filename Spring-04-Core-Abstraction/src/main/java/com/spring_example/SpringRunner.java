775e93ad3d69f9b3ab914e9b9d8896370ef94d22
ZB <zeynepbstg@hotmail.com>
2023-02-28 22:06:23 -0500

    Spring-04 is done

---
+++ Spring-04-Core-Abstraction/src/main/java/com/spring_example/SpringRunner.java
@@ -0,0 +1,21 @@
+package com.spring_example;
+
+import com.spring_example.config.ProjectConfig;
+import com.spring_example.model.Comment;
+import com.spring_example.service.CommentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SpringRunner {
+    public static void main(String[] args) {
+
+        Comment comment = new Comment();
+        comment.setAuthor("Johnson");
+        comment.setText("Spring Framework");
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        CommentService commentService = context.getBean(CommentService.class);
+        commentService.publishComment(comment);
+    }
+}
---
+++ Spring-04-Core-Abstraction/src/main/java/com/spring_example/service/CommentService.java
@@ -0,0 +1,31 @@
+package com.spring_example.service;
+
+import com.spring_example.model.Comment;
+import com.spring_example.proxy.CommentNotificationProxy;
+import com.spring_example.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Scope("prototype")
+//@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+@Lazy
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+    private final CommentNotificationProxy commentNotificationProxy;
+
+    public CommentService(CommentRepository commentRepository, @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
+    public void publishComment(Comment comment){
+        commentRepository.storeComment(comment);
+        commentNotificationProxy.sendComment(comment);
+
+    }
+
+
+}
