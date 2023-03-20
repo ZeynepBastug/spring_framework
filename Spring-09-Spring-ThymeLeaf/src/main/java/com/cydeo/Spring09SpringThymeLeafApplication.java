3a7b6ec844d632e7c465a8aced114244ee263b0f
ZB <zeynepbstg@hotmail.com>
2023-03-20 18:50:48 -0400

    spring 9 module created

---
+++ Spring-09-Spring-ThymeLeaf/src/main/java/com/cydeo/Spring09SpringThymeLeafApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring09SpringThymeLeafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring09SpringThymeLeafApplication.class, args);
+    }
+
+}
