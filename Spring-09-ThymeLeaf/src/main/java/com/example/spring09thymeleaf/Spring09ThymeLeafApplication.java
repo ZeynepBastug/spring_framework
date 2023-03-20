480b1c148eefae99a73d56827afd85e86b989c5d
ZB <zeynepbstg@hotmail.com>
2023-03-20 18:29:09 -0400

    spring 9 added

---
+++ Spring-09-ThymeLeaf/src/main/java/com/example/spring09thymeleaf/Spring09ThymeLeafApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring09thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring09ThymeLeafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring09ThymeLeafApplication.class, args);
+	}
+
+}
